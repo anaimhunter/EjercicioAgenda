efc05bccfacb0c15de2f8a6590b5a766091f7c45
anaimhunter <anaimhunter@hotmail.com>
2017-10-25 11:03:46 -0500

    Agrego un agenda con contactos y se muestran

--- src/AplicacionTerminal.java
+++ src/AplicacionTerminal.java
@@ -3,7 +3,13 @@
  */
 public class AplicacionTerminal {
     public static void main(String[] args) {
-        Contacto c1 = new Contacto("Ana","UASLP",4445816296L);
-
+        Contacto c1 = new Contacto("Ana","Montes",4445816296L);
+        Contacto c2 = new Contacto("Silvia"," Uaslp",4445621967L);
+        Contacto c3 = new Contacto("Vallejo","labo ",4445789245L);
+        Agenda agenda = new Agenda();
+        agenda.agregaContacto(c1);
+        agenda.agregaContacto(c2);
+        agenda.agregaContacto(c3);
+        agenda.imprimerTodo();
     }
 }
